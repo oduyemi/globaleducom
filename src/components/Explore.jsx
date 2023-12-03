0ededd654cbfd8a0dcce9dfd419116ec8efeee92
Yemi Oduyemi <yhermii@gmail.com>
2023-12-03 13:00:05 +0100

    replace images

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -7,7 +7,7 @@ import "../css/theme-rtl.css";
 
 export const Explore = () => {
     return(
-        <Box  className="bg-secondary">
+        <Box className="bg-secondary my-5">
         <Box className="container">
           <Box className="row g-3">
             <Typography variant="h1" className="header-title-explore display-4 header text-start mb-5">
@@ -15,14 +15,14 @@ export const Explore = () => {
             </Typography>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/data-science.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Data Science</Typography>
-                      <a className="stretched-link explore-link" href="#!">78 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Data Science</Typography>
+                      <a className="stretched-link explore-link text-primary" href="#!">78 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -30,14 +30,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/business.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Business &amp; Management</Typography>
-                      <a className="stretched-link explore-link" href="#!">67 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Business &amp; Management</Typography>
+                      <a className="stretched-link explore-link" href="#!">67 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -45,14 +45,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/language.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Language</Typography>
-                      <a className="stretched-link explore-link" href="#!">85 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Language</Typography>
+                      <a className="stretched-link explore-link" href="#!">85 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -60,14 +60,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/information.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Information Technology</Typography>
-                      <a className="stretched-link explore-link" href="#!">98 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Information Technology</Typography>
+                      <a className="stretched-link explore-link" href="#!">98 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -75,14 +75,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/film.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Modernism in Oriental Art</Typography>
-                      <a className="stretched-link explore-link" href="#!">68 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Modernism in Oriental Art</Typography>
+                      <a className="stretched-link explore-link" href="#!">68 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -90,14 +90,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/math.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Compositions in Visual Art</Typography>
-                      <a className="stretched-link explore-link" href="#!">878 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Compositions in Visual Art</Typography>
+                      <a className="stretched-link explore-link" href="#!">878 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -105,14 +105,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                             <img className="educom-icon" src={require("../assets/images/icons/health.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Health &amp; Medical</Typography>
-                      <a className="stretched-link explore-link" href="#!">79 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-warning mb-0">Health &amp; Medical</Typography>
+                      <a className="stretched-link explore-link" href="#!">79 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
@@ -120,14 +120,14 @@ export const Explore = () => {
             </Box>
             <Box className="col-sm-6 col-lg-3">
               <Box className="card card-hover h-100">
-                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4">
+                <Box className="card-body d-flex align-items-center px-4 px-lg-2 px-xl-4 bg-light">
                   <Box className="d-flex align-items-center">
-                    <Box className="icon-box">
+                    <Box className="icon-box bg-light">
                         <img className="educom-icon" src={require("../assets/images/icons/design.png")} alt="explore" />
                     </Box>
                     <Box className="flex-grow-1 ms-3">
-                      <Typography variant="h4" className="fs-lg-1 text-light mb-0">Design &amp; Creative</Typography>
-                      <a className="stretched-link explore-link" href="#!">37 Courses</a>
+                      <Typography variant="h4" className="fs-lg-1 text-primary mb-0">Design &amp; Creative</Typography>
+                      <a className="stretched-link explore-link" href="#!">37 resource materials</a>
                     </Box>
                   </Box>
                 </Box>
--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/theme.css";
 import "../css/theme-rtl.css";
@@ -11,26 +11,30 @@ export const Resources = () => {
         <Box className="py-6">
         <Box className="container">
           <Box className="row">
-            <h1 className="header-title display-4 header text-start mb-5"> TOP RESOURCE MATERIALS</h1>
+            <Typography variant="h1" className="header-title display-4 header text-start mb-5"> TOP RESOURCES</Typography>
             <Box className="col-sm-6 col-lg-3 mb-4">
               <Box className="card border-0 h-100">
-                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/art-masterclass.png")} alt="courses" />
+                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/taxation.jpg")} alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/moma.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">The Professional Art Masterclass</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">The Museum of Modern Art</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Company Law Dissertation</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">
+                    Should the OECD&apos;s Model Tax Convention on Income and on Capital 2010 be ratified into UK Law?
+                  </a>
                 </Box>
               </Box>
             </Box>
             <Box className="col-sm-6 col-lg-3 mb-4">
               <Box className="card border-0 h-100">
-                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/premiere.png")} alt="courses" />
+                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/med.jpg")} alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/moma.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Paid</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">Premiere Pro CC for Beginners</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">The Museum of Modern Art</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Medicine Research</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Genetic Engineering</a>
                 </Box>
               </Box>
             </Box>
@@ -38,10 +42,13 @@ export const Resources = () => {
               <Box className="card border-0 h-100">
                 <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/electricity.png")} alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/y.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">Electricity & Magnetism</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Duke University</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Electricity & Magnetism</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">
+                    A Microgrid for Optimal Energy Consumption depending on Wind-Driven DFIG, DG, and Solar Photovoltaic Arrays
+                  </a>
                 </Box>
               </Box>
             </Box>
@@ -49,54 +56,65 @@ export const Resources = () => {
               <Box className="card border-0 h-100">
                 <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/chemistry.png")} alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/school.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">School chemistry beta</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Duke University</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Medical Research</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Medical humanitarian missions in the developing world</a>
                 </Box>
               </Box>
             </Box>
             <Box className="col-sm-6 col-lg-3 mb-4">
               <Box className="card border-0 h-100">
                 <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/oriental-art.png")} alt="courses" />
-                  <Box className="course-logo"><img src={require("../assets/images/gallery/moma.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
-                </Box>
-                <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">Modernism in Oriental Art</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">The Museum of Modern Art</a>
-                </Box>
-              </Box>
-            </Box>
-            <Box className="col-sm-6 col-lg-3 mb-4">
-              <Box className="card border-0 h-100">
-                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/visual-art.png")}alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/cal-arts.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">Compositions in Visual Art</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">The Museum of Modern Art</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Modern Art</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Abstract Expressionism</a>
                 </Box>
               </Box>
             </Box>
             <Box className="col-sm-6 col-lg-3 mb-4">
               <Box className="card border-0 h-100">
-                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/physics.png")} alt="courses" />
+                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/lang.jpg")}alt="courses" />
+                  <Box className="course-logo"><img src={require("../assets/images/gallery/y.png")} alt="logo" /></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
+                </Box>
+                <Box className="card-body px-0 py-3">
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Sociolinguistics</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Analyzing language attrition among most English speakers</a>
+                </Box>
+              </Box>
+            </Box>
+            <Box className="col-sm-6 col-lg-3 mb-4">
+              <Box className="card border-0 h-100">
+                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/data.jpg")} alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/school-physics.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">High school physics</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">The Museum of Modern Art</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">Data Science &amp; Analytics</Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">
+                    Prediction and Classification of COVID-19 Death and Recovered cases using ML
+                  </a>
                 </Box>
               </Box>
             </Box>
             <Box className="col-sm-6 col-lg-3 mb-4">
               <Box className="card border-0 h-100">
-                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/davinci.png")} alt="courses" />
+                <Box className="position-relative"><img className="w-100" src={require("../assets/images/gallery/pm.jpg")} alt="courses" />
                   <Box className="course-logo"><img src={require("../assets/images/gallery/moma.png")} alt="logo" /></Box>
-                  <Box className="ps-6"><span className="badge bg-primary rounded-0">Free</span></Box>
+                  <Box className="ps-6"><span className="badge bg-warning rounded-0">Free</span></Box>
                 </Box>
                 <Box className="card-body px-0 py-3">
-                  <h5 className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1">Color Grading with Da Vinci</h5><a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">Duke University</a>
+                  <Typography variant="h5" className="mb-0 font-sans-serif fw-bold fs-md-0 fs-lg-1 text-warning">
+                    Project Management
+                  </Typography>
+                  <a className="text-muted fs--1 stretched-link text-decoration-none" href="#!">
+                    Stakeholder Approach to Successful Adoption of Projects
+                  </a>
                 </Box>
               </Box>
             </Box>
