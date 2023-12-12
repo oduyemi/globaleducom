f00045cd9a9284a6f544eaca570e58709615aece
Yemi Oduyemi <yhermii@gmail.com>
2023-12-12 11:01:01 +0100

    request endpoints adjusted

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -28,7 +28,7 @@ export const RegisterForm = () => {
   const handleRegister = async () => {
     try {
       setLoading(true);
-      const response = await axios.post('https://globaleducomm.com/api/register', formData);
+      const response = await axios.post('https://globaleducomm.com/api/send/register', formData);
       console.log(response.data);
     } catch (error) {
       console.error('Error during registration:', error);
