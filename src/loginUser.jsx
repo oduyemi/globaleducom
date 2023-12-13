44f282c31196dd003f8184c2e567d48a2e093776
Yemi Oduyemi <yhermii@gmail.com>
2023-12-13 16:22:07 +0100

    updated8

--- src/loginUser.jsx
+++ src/loginUser.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const loginUser = async (userId) => {
+  const userIdInt = parseInt(userId, 10);
   try {
-    const response = await axios.get(`https://globaleducomm.com/api/users/user/${userId}`); 
+    const response = await axios.get(`https://globaleducomm.com/api/users/user/${userIdInt}`); 
     return response.data;
   } catch (error) {
     console.error("Error fetching user data:", error);
