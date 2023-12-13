87de144ae17cbdd9fa619211143ee40338dfbeef
Yemi Oduyemi <yhermii@gmail.com>
2023-12-13 15:13:20 +0100

    QueryClient integration

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'; // Optional, for devtools
 import ErrorBoundary from './ErrorBoundary';
+
+
+
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools />
+        </BrowserRouter>
+      </ErrorBoundary>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
