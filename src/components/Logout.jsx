5de8aa3bd15dece23c35d16043f076ab7c041d16
Yemi Oduyemi <yhermii@gmail.com>
2023-12-13 17:23:29 +0100

    logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -4,9 +4,9 @@ import { clearCookie } from '../../CookieUtils';
 export const handleLogout = () => {
     if (getCookie('userId')) {
         clearCookie('userId');
-        window.location.href = '/login';
-      } else {
         window.location.href = '/';
+      } else {
+        window.location.href = '/dashboard';
       }
     };
   
