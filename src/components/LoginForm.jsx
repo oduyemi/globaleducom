e6bcb40af1f45cf5c020a3765f9ba6d11125b8ed
Yemi Oduyemi <yhermii@gmail.com>
2023-12-14 09:49:39 +0100

    edit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -54,7 +54,7 @@ export const LoginForm = () => {
       if (flashType === "success") {
         setFlashMessage(flashMessage);
         
-        setCookie('userId', response.data.userId, { expires: new Date(Date.now() + 3600000) });
+        setCookie('userId', response.data.userId, { expires: new Date(Date.now() + 3600000), secure: true, path: '/' });
   
         console.log("Login successful. Redirecting to dashboard...");
         window.location.href = "/dashboard";
--- src/loginUser.jsx
+++ src/loginUser.jsx
@@ -1,16 +1,19 @@
 import axios from "axios";
 
 const loginUser = async (userId) => {
+  console.log('Fetching user data for userId:', userId);
+
   const userIdInt = parseInt(userId, 10);
   try {
     const response = await axios.get(`https://globaleducomm.com/api/users/user/${userIdInt}`);
+    console.log('User data fetched successfully:', response.data);
     return response.data;
   } catch (error) {
-    console.log(typeof userId)
-    console.error("Error fetching user data:", error);
-    throw new Error("Failed to fetch user data");
+    console.log('Error fetching user data:', error);
+    throw new Error('Failed to fetch user data');
   }
 };
+
 
 export default loginUser;
 
