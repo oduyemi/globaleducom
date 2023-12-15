cb7f7f91fcfded9beb3d2fa3790d6d4723c8fc7d
Yemi Oduyemi <yhermii@gmail.com>
2023-12-15 12:45:51 +0100

    dashboard rendering

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 import { Dashboard } from "../../views/pages/Dashboard";
-import { Typography } from "@mui/material";
-import { useQuery } from "react-query";
-import loginUser from "../../loginUser";
 import { FooterBottom } from "../../components/FooterBottom";
-import { Navigate } from "react-router-dom"; // Import Navigate
+import useAuth from "../../useAuth";
 
-const DashboardPage = ({ userId }) => {
-  // const { data: userData, isLoading, isError } = useQuery(
-  //   ["userData", userId],
-  //   () => loginUser(userId),
-  //   {
-  //     enabled: !!userId,
-  //   }
-  // );
 
-  // if (isLoading) {
-  //   return <Typography>Loading user data...</Typography>;
-  // }
-
-  // if (isError || !userData) {
-  //   return <Navigate to="/login" />;
-  // }
-
+const DashboardPage = () => {
+  const { userId } = useAuth();
+  
   return (
     <>
-      <Dashboard />
+      <Dashboard userId={userId} />
       <FooterBottom />
     </>
   );
