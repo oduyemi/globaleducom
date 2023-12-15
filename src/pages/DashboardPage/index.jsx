bb726af640d01784d5e932fbeb57684c9c0270a9
Yemi Oduyemi <yhermii@gmail.com>
2023-12-15 14:05:56 +0100

    edit3

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -3,10 +3,9 @@ import { Dashboard } from "../../views/pages/Dashboard";
 import { FooterBottom } from "../../components/FooterBottom";
 import useAuth from "../../useAuth";
 
-
 const DashboardPage = () => {
   const { userId } = useAuth();
-  
+
   return (
     <>
       <Dashboard userId={userId} />
