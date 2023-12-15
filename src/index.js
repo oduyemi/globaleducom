1a3f2427c56090e0513c09538a49f3ea49a7b6b9
Yemi Oduyemi <yhermii@gmail.com>
2023-12-15 13:13:59 +0100

    SessionProvider update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
+// index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'; // Optional, for devtools
+import { ReactQueryDevtools } from 'react-query/devtools';
 import ErrorBoundary from './ErrorBoundary';
 import App from './App';
-
-
-
+import SessionProvider from './SessionProvider';  // Import SessionProvider
 
 const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
-  <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
-    <ReactQueryDevtools />
-  </React.StrictMode>
-</QueryClientProvider>
-
+    <React.StrictMode>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <SessionProvider>
+            <App />
+          </SessionProvider>
+        </BrowserRouter>
+      </ErrorBoundary>
+      <ReactQueryDevtools />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
-
-
-
