e2034d5fcbdc53b6fb613838280c0ce28ad6a51d
Yemi Oduyemi <yhermii@gmail.com>
2023-12-18 06:46:02 +0100

    edit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -121,8 +121,8 @@ export const LoginForm = () => {
               />
             </div>
             <div className="text-center pt-1 mb-2 pb-1">
-              <Button onClick={handleLogin} className="mb-2 w-100">
-                {/* {loading ? "Hang on..." : "Login"} */}
+              <Button onClick={handleLogin} className="mb-2 w-100" disabled={loading}>
+                {loading ? "Hang on..." : "Login"}
               </Button>
               <Link className="text-muted" to="#!">
                 Forgot password?
